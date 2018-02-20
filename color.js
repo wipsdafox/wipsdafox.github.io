026badcb6cb084f1dfbcbed97ff750412982b409
wips <33335084+wipsdafox@users.noreply.github.com>
2018-02-20 09:31:43 -0700

    Update color.js

--- color.js
+++ color.js
@@ -1,6 +1,6 @@
 //Color.js
 var responses = new Object(),
-  numbers = [86, 12, 1, 11, 1, 0, 1234567, 42],
+  numbers = [86, 12, 1, 11, 1, 0, 1234567, 42, 86, 8.4],
   letters = ["z", "a", "b", "c", "p"],
   colors = ["red", "yellow", "purple", "green", "yellow", "yellow", "blurple", "blue", "green", "purple"],
   other = ["burr", "7L", ": middle-finger-emoji :", "2b", "2b"];
